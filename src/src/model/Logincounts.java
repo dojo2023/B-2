bba57fa7eba641077e0ac1a9e42e8092bf289aa1
dojo2023-hayano <130334524+dojo2023-hayano@users.noreply.github.com>
2023-06-23 10:26:48 +0900

    logincounts.java_0623

--- src/src/model/Logincounts.java
+++ src/src/model/Logincounts.java
@@ -1,5 +1,57 @@
 package model;
 
-public class Logincounts {
+import java.io.Serializable;
+
+public class Logincounts implements Serializable {
+	private int id;
+	private int user_id;
+	private String created_at;
+	private String update_at;
+	
+	public Logincounts(int id, int user_id, String created_at, String update_at) {
+		this.id = id;
+		this.user_id = user_id;
+		this.created_at = created_at;
+		this.update_at = update_at;
+	}
+	
+	public Logincounts() {
+		this.id = 0;
+		this.user_id = 0;
+		this.created_at = "";
+		this.update_at = "";
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public String getUpdate_at() {
+		return update_at;
+	}
+
+	public void setUpdate_at(String update_at) {
+		this.update_at = update_at;
+	}
 
 }
