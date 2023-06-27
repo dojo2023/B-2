8f1e88298cb7240b89e60638ba7f4505effcf989
dojo2023-hayano <130334524+dojo2023-hayano@users.noreply.github.com>
2023-06-27 13:28:50 +0900

    hayano_0627_1

--- src/WebContent/WEB-INF/jsp/mur_list.jsp
+++ src/WebContent/WEB-INF/jsp/mur_list.jsp
@@ -19,7 +19,7 @@
 
 	<!-- まめぇまめぇのロゴ画像 -->
 	<div class="logo">
-		<img src="logo.png" alt="ロゴ">
+		<a href="/BtwoB/TopServlet"><img src="/BtwoB/img/logo.png" alt="まめぇまめぇ" ></a>
 	</div>
 	<nav class="right-align">
 		<ul class="menu">
--- src/WebContent/js/graph.js
+++ src/WebContent/js/graph.js
@@ -1,16 +1,38 @@
 //描画エリアの設定
 let lineCtx = document.getElementById("Chart");
 // 円グラフの設定
+let initialLabelsData = ['仕事', '友達', '家族', '恋愛', '自分', 'その他'];
+let initialColors = ['#B0C4DE', '#66CDAA', '#FFDEAD', '#FFB6C1', '#F0E68C', '#DDA0DD'];
+
+
+/*let tagColorMap = {
+  仕事: '#B0C4DE',
+  友達: '#66CDAA',
+  家族: '#FFDEAD',
+  恋愛: '#FFB6C1',
+  自分: '#F0E68C',
+  その他: '#DDA0DD',
+};*/
+
 let lineConfig = {
     type: 'pie',
     data: {
-        labels: labelsData,
+       /*labels: labelsData,*/
+       /*labels: ['仕事', '友達', '家族', '恋愛', '自分', 'その他'],*/
+       
 
-        datasets: [{
+       /*datasets: [{
             data: datesValue,
             borderColor: '#ffffff',
-            backgroundColor: ['#B0C4DE','#66CDAA','#FFDEAD','#FFB6C1','#F0E68C','#DDA0DD'],
+            backgroundColor: ['#B0C4DE','#66CDAA','#FFDEAD','#FFB6C1','#F0E68C','#DDA0DD'],*/
+            
+            
+              labels: initialLabelsData,
+        datasets: [{
+            data: datesValue,
+            borderColor: '#FFFFFF',
+            backgroundColor: initialColors,
         }],
-    },
-};
+        },
+        };
 let lineChart = new Chart(lineCtx, lineConfig);
